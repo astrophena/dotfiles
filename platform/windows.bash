5d9f3b3b17a647f2f93305da10743d31616bb9c8
Ilya Mateyko <gitlab@inbox.astrophena.me>
2019-11-03 22:45:32 +0300

    Windows: Add Go

--- platform/windows.bash
+++ platform/windows.bash
@@ -1,3 +1,7 @@
 alias word="/c/Program\ Files/Microsoft\ Office/Office14/WINWORD.EXE"
 
+export GOROOT="$HOME/Documents/Go"
+export GOPATH="$HOME/src/go"
+export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"
+
 export GNUPGHOME="$APPDATA/gnupg"
