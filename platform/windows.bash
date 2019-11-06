5dcca7b16a3d7b240dd3b6da0aeaac1ec0b6002e
Ilya Mateyko <gitlab@inbox.astrophena.me>
2019-11-06 21:12:15 +0300

    update platform/windows

--- platform/windows.bash
+++ platform/windows.bash
@@ -1,7 +1,9 @@
 alias word="/c/Program\ Files/Microsoft\ Office/Office14/WINWORD.EXE"
 
-export GOROOT="$HOME/Documents/Go"
-export GOPATH="$HOME/src/go"
-export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"
+[[ -d "$HOME/Documents/Go" ]] && {
+  export GOROOT="$HOME/Documents/Go"
+  export GOPATH="$HOME/src/go"
+  export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"
+}
 
 export GNUPGHOME="$APPDATA/gnupg"
