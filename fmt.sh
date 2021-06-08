0f10e15f684993a91c391f964392c3d9e2527003
Ilya Mateyko <me@astrophena.name>
2021-06-08 20:08:55 +0300

    fmt.sh: include additional files

---
+++ fmt.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Include here files that shfmt can't detect.
+ADDITIONAL_FILES=(
+	"tag-bash/bashrc"
+	"tag-neofetch/config/neofetch/config.conf"
+	"rcrc"
+)
+
+# Error with diff in CI, format everywhere else.
+ARGS=""
+if [[ ! -z "${CI:-}" ]]; then
+	ARGS+="-d"
+else
+	ARGS+="-w"
+fi
+
+shfmt "$ARGS" "${ADDITIONAL_FILES[@]}" .
